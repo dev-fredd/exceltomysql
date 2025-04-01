16f14ac411a90a8ecbf55917026652cf7cf380d5
dev-fredd <ingfredd.dev@gmail.com>
2025-04-01 15:24:48 -0500

    fix(core): delete FileUploadRequest unused properties

--- Application/Dtos/Requests/FileUploadRequest.cs
+++ Application/Dtos/Requests/FileUploadRequest.cs
@@ -2,8 +2,6 @@ namespace Exceltomysql.Application.Dtos.Requests
 {
     public class FileUploadRequest
     {
-        public string UploaderName { get; set; }
-        public string UploaderAdress { get; set; }
         public IFormFile File { get; set; }
     }
 }
